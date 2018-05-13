00f65499d6cd61aec6f0cf86455ac90462e7c074
karlosdiaz99 <34726057+karlosdiaz99@users.noreply.github.com>
2018-05-13 02:36:58 -0500

    Update twitter.js

--- KIDC-KIDC/twitter.js
+++ KIDC-KIDC/twitter.js
@@ -11,25 +11,6 @@ function buscarTweets(){
         count: 100
         //max_id:0
     };
-    cb.__call(
-        "search_tweets",
-        params,
-        function(response){
-            console.log("cantidad: " + response.statuses.length);
-
-          console.log("-------------------");
-          var _cuerpo =  document.getElementById("tcuerpot");
-                      console.log(response);
-          for(var i=0;i<response.statuses.length;i++){
-            //console.log("i: "+ titulo);
-            var titulo = response.statuses[i].text;
-            if(response.statuses[i].coordinates!==null){
-                var lat = response.statuses[i].coordinates.coordinates[1];
-                var lng = response.statuses[i].coordinates.coordinates[0];
-                marcarTweets(lat,lng, titulo);
-              }
-          }
-        }
-    );
+    
 }
 
