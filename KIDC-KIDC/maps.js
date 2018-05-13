8ef9fb54b63f7dd11cd55d7b8a1e5852fed15178
karlosdiaz99 <34726057+karlosdiaz99@users.noreply.github.com>
2018-05-13 03:28:04 -0500

    Update maps.js

--- KIDC-KIDC/maps.js
+++ KIDC-KIDC/maps.js
@@ -5,16 +5,12 @@ function dibujarMapa(){
       center: uluru
     });
 }
-
 function crearMarcadores(latitud, longitud, identificador){
-
 	var _iframe = document.createElement("iframe");
         _iframe.src= "//www.youtube.com/embed/" + identificador;
         _iframe.width = "320";
         _iframe.height= "200";
         _iframe.class= "video w100"; 
-
-
     var uluru = {lat: latitud, lng: longitud};
     var infowindow = new google.maps.InfoWindow({
           content: _iframe
@@ -27,7 +23,6 @@ function crearMarcadores(latitud, longitud, identificador){
           infowindow.open(map, marker);
         });
 }
-
 function marcarTweets(latitud, longitud, informacion){
         var image = "twitter-50x50.png"
      var uluru = {lat: latitud, lng: longitud};
@@ -39,11 +34,8 @@ function marcarTweets(latitud, longitud, informacion){
           map: map,
           icon: image
         });
-
         marker.addListener('click', function() {
           infowindow.open(map, marker);
         });
-      
-
         console.log("posicion: "+latitud + longitud);
     }
