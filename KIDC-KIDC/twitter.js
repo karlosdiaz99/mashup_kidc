32d35dcfbab6fb788182642c8885030ec91566fd
karlosdiaz99 <34726057+karlosdiaz99@users.noreply.github.com>
2018-05-13 03:30:14 -0500

    Update twitter.js

--- KIDC-KIDC/twitter.js
+++ KIDC-KIDC/twitter.js
@@ -3,25 +3,21 @@ cb.setConsumerKey ( "XDRdKxPJOK8ClzUVl6RKAlaRc" , "23GZxIl0kW11fT4o7zFFOI4vTVwF8
 cb.setToken ( "842057213246087173-ZdEsCIi7gm0fUF166Rw4KCSqfBi0Jw9", "cKlTbMJ4h61LxRBsuKomryy94bKhgvwnxqJxeYBWAFuBk");
 
 function buscarTweets(){
-    //var query = document.getElementById('query').value;
     var q = document.getElementById("query").value;
     console.log(q);
     var params = {
         q: q,
         count: 100
-        //max_id:0
     };
     cb.__call(
         "search_tweets",
         params,
         function(response){
             console.log("cantidad: " + response.statuses.length);
-
           console.log("-------------------");
           var _cuerpo =  document.getElementById("tcuerpot");
                       console.log(response);
           for(var i=0;i<response.statuses.length;i++){
-            //console.log("i: "+ titulo);
             var titulo = response.statuses[i].text;
             if(response.statuses[i].coordinates!==null){
                 var lat = response.statuses[i].coordinates.coordinates[1];
@@ -32,20 +28,16 @@ function buscarTweets(){
             var _fila = document.createElement("tr");
             var _celda = document.createElement("td");
             var _pararfo = document.createElement("p");
-            //console.log("dentro del for");
             _pararfo.textContent = titulo;
             _celda.appendChild(_pararfo);
             _fila.appendChild(_celda);
-
             var _celda = document.createElement("td");
             var _botonAgregar = document.createElement("button");
             _botonAgregar.textContent="Retwit";
-            
             _botonAgregar.id = i;
             _botonAgregar.addEventListener("click", function(obj_evento){funcionAgregar(titulo);},false);
             _celda.appendChild(_botonAgregar);
             _fila.appendChild(_celda); 
-
             _cuerpo.appendChild(_fila);
           }
         }
