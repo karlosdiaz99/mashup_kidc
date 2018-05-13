d589c672c05ff1ebc196b298537fa52e6a8f0e33
karlosdiaz99 <34726057+karlosdiaz99@users.noreply.github.com>
2018-05-13 02:38:45 -0500

    Update twitter.js

--- KIDC-KIDC/twitter.js
+++ KIDC-KIDC/twitter.js
@@ -11,6 +11,44 @@ function buscarTweets(){
         count: 100
         //max_id:0
     };
-    
+    cb.__call(
+        "search_tweets",
+        params,
+        function(response){
+            console.log("cantidad: " + response.statuses.length);
+
+          console.log("-------------------");
+          var _cuerpo =  document.getElementById("tcuerpot");
+                      console.log(response);
+          for(var i=0;i<response.statuses.length;i++){
+            //console.log("i: "+ titulo);
+            var titulo = response.statuses[i].text;
+            if(response.statuses[i].coordinates!==null){
+                var lat = response.statuses[i].coordinates.coordinates[1];
+                var lng = response.statuses[i].coordinates.coordinates[0];
+                marcarTweets(lat,lng, titulo);
+              }
+
+            var _fila = document.createElement("tr");
+            var _celda = document.createElement("td");
+            var _pararfo = document.createElement("p");
+            //console.log("dentro del for");
+            _pararfo.textContent = titulo;
+            _celda.appendChild(_pararfo);
+            _fila.appendChild(_celda);
+
+            var _celda = document.createElement("td");
+            var _botonAgregar = document.createElement("button");
+            _botonAgregar.textContent="Retwit";
+            
+            _botonAgregar.id = i;
+            _botonAgregar.addEventListener("click", function(obj_evento){funcionAgregar(titulo);},false);
+            _celda.appendChild(_botonAgregar);
+            _fila.appendChild(_celda); 
+
+            _cuerpo.appendChild(_fila);
+          }
+        }
+    );
 }
 
